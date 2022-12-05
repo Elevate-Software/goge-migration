0b1eaf5c963be690ff544b70cf0bb95c3a207aa1
sworezack <59543930+sworezack@users.noreply.github.com>
2022-12-04 21:28:36 -0700

    Updates to add responsive styling

--- components/Migration.tsx
+++ components/Migration.tsx
@@ -1,14 +1,41 @@
 import React from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom/client";
+import { ethers } from "ethers";
+
+const provider = new ethers.providers.JsonRpcProvider(
+    process.env.NEXT_PUBLIC_GOGE_JSON_RPC_PROVIDER
+);
+
+const [balance, setBalance] = useState() as any;
+const [account, setAccount] = useState(null);
+
+async function getBalance() {
+  const ethereum = (window as any).ethereum;
+
+  if (ethereum) {
+    const [account] = await ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(account);
+
+    const balance = Number(
+      ethers.utils.formatEther(await provider.getBalance(account))
+    );
+    setBalance(balance.toFixed(2));
+  } else {
+    console.log("Please install Wallet");
+  }
+}
 
 const Migration = () => {
     return (
         <>
             <div className="top-migration-section px-10">
-                <div className="w-2/6 py-7 m-auto text-center font-semibold"><span>Goge Migration Page</span><br /><span className="text-sm">Migrate your v1 tokens for v2 tokens.</span></div>
+                <div className="w-2/6 xs:w-5/6 sm:w-4/6 md:w-2/6 lg:w-2/6 xl:w-2/6 py-7 m-auto text-center font-semibold"><span>Goge Migration Page</span><br /><span className="text-sm">Migrate your v1 tokens for v2 tokens.</span></div>
             </div>
             <div className="flex h-screen">
-                <div className="m-auto w-1/6">
+                <div className="m-auto w-1/6 xs:w-5/6 sm:w-4/6 md:w-1/6 lg:w-1/6 xl:w-1/6">
                     <div className="migrate-box">
                         <div className="px-4 py-5 sm:p-6">
                             <div className="mt-2 max-w-xl text-sm">
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,21 @@ import styles from '../styles/Home.module.css'
 import Migration from '../components/Migration'
 import Logo1 from "../public/goge_logo.png";
 import Logo2 from "../public/goge_logo_2.png";
-import Rainbow from "../public/goge_rainbow.png";
+import Rainbow from "../public/rainbow.png";
 
 export default function Home() {
   return (
     <>
-      <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 goge-navbar shadow sm:items-baseline w-full">
+      <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-1 px-6 goge-navbar shadow sm:items-baseline w-full">
           <div className="mb-2 sm:mb-0">
-          <Image src={Logo2} className="inline" alt="MetaRaft" /><Image className="inline" src={Logo1} alt="MetaRaft" />
+            <Image src={Logo2} className="inline" alt="goge" /><Image className="inline" src={Logo1} alt="dog" />
+          </div>
+          <div className="mb-2 sm:mb-0">
+            <Image src={Rainbow} className="inline" alt="rainbow" />
           </div>
           <div className="mt-5 flex flex-col items-center">
-          <div className='inline-flex m-auto content-center migrate-button px-4 py-2 sm:text-sm'>Token Address</div>
-      </div>
+            <div className='inline-flex m-auto content-center migrate-button px-4 py-2 sm:text-sm'>Token Address</div>
+          </div>
       </nav>
       <div>
         <Migration />
