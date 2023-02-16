115f818f9bb1baa23ee49cb00e698f4026b2aa59
Andrew Gucci <athoma67@asu.edu>
2023-02-16 14:08:24 -0700

    Developed rough migration process using Wagmi.

--- components/Migration.tsx
+++ components/Migration.tsx
@@ -1,150 +1,122 @@
 import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
-import ReactDOM from "react-dom/client";
-import { BigNumber, Contract, ethers } from "ethers";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 import GogeToken1 from "../pages/GogeTokenV1.json";
 import GogeToken2 from "../pages/GogeTokenV2.json";
-import useWindowSize from 'react-use/lib/useWindowSize';
 import Confetti from 'react-confetti';
 import Image from 'next/image'
 import Logo1 from "../public/goge_logo.png";
 import Logo2 from "../public/goge_logo_2.png";
 import Rainbow from "../public/rainbow.png";
-import { truncate } from 'truncate-ethereum-address';
-import { stat } from "fs";
-import { Component } from "react";
-// Web3Modal import
-import { Web3Modal } from "@web3modal/react";
-import { EthereumClient, modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
-import { Web3Button, Web3NetworkSwitch } from '@web3modal/react'
 
-
-// Wagmi import
-import { configureChains, createClient, WagmiConfig, useAccount, useBalance, useContractRead, useContractWrite, usePrepareContractWrite } from 'wagmi'
-import { bscTestnet, bsc } from "wagmi/chains";
-
-if(!process.env.WALLET_CONNECT_PROJECT_ID){
-    throw new Error('You need to provide WALLET_CONNECT_PROJECT_ID env variable')
-}
-
-const projectId = process.env.WALLET_CONNECT_PROJECT_ID
-
-
-const chains = [bscTestnet]
-const { provider } = configureChains(chains, [walletConnectProvider({ projectId })])
-export const wagmiClient = createClient({
-  autoConnect: true,
-  connectors: modalConnectors({
-    appName: 'GogeMigration',
-    chains
-  }),
-  provider
-})
-
-export const ethereumClient = new EthereumClient(wagmiClient, chains)
-
-//const provider = new ethers.providers.JsonRpcProvider("https://rpc.ankr.com/bsc_testnet_chapel");
-let account = ''
+// Web3 connect wallet import
+import { Web3Button } from '@web3modal/react'
+// Wagmi imports
+import { useAccount, useBalance, useContractWrite, usePrepareContractWrite } from 'wagmi'
 
 // setup Migration component
 const Migration = () => {
-  const [enabledBalance, setEnabledBalance] = useState(false);
-  const [status, setStatus] = useState("");
-  const [connected, setConnected] = useState(false);
-  const [migrated, setMigrated] = useState(false);
-  const [wallet, setWallet] = useState("");
-  const [web3Provider, setProvider] = useState(null) as any;
-  const { width, height } = useWindowSize();
   const [balanceV1, setBalanceV1] = useState("0.0");
   const [balanceV2, setBalanceV2] = useState("0.0");
-  const [ready, setReady] = useState(false)
 
   // Get address using Wagmi
-  const { address: addressWallet } = useAccount({
+  const { isConnected: isConnected, address: wallet } = useAccount({
     onConnect(data) {
-      console.log('Success addressWallet:', data.address);
+      console.log('Success wallet:', data.address);
     },
     onDisconnect() {
       console.log('Disconnected')
     },
-  }) as any;
+  });
 
   // Get V1 balance using Wagmi
-  const { data: balv1 } = useBalance({
-    address: addressWallet,
-    token: '0xf89c10e67b2F8bDd2a44cF4E081378e0EAA5C428',
-    enabled: enabledBalance,
-    formatUnits: 'ether',
+  useBalance({
+    address: wallet,
+    token: "0xf89c10e67b2F8bDd2a44cF4E081378e0EAA5C428",
+    enabled: isConnected,
     onSuccess(data) {
-      setBalanceV1(data.formatted.substring(0,25))
-      console.log('Success balanceWallet:', balanceV1)
+      setBalanceV1(data.formatted)
+      console.log('Success V1 balance:', data.formatted)
     },
     onError(error) {
-      console.log('Error balanceWallet', error)
+      console.error('Error V1 balance', error)
     },
   })
 
   //Get V2 balance using Wagmi
-  const { data: balv2 } = useBalance({
-    address: addressWallet,
-    token: '0xf89c10e67b2F8bDd2a44cF4E081378e0EAA5C428',
-    enabled: enabledBalance,
-    formatUnits: 'ether',
+  useBalance({
+    address: wallet,
+    token: "0x1618efC9867F3Bd7D2bf80ce5f7E6174Fd3bEf96",
+    enabled: isConnected,
     onSuccess(data) {
-      setBalanceV1(data.formatted.substring(0,25))
-      console.log('Success balanceWallet:', balanceV2)
+      setBalanceV2(data.formatted)
+      console.log('Success V2 balance:', data.formatted)
     },
     onError(error) {
-      console.log('Error balanceWallet', error)
+      console.error('Error V2 balance', error)
     },
   })
 
-  useEffect(() => {
-    if(!!addressWallet) {
-      setEnabledBalance(true);
-      setConnected(true);
-    } else {
+  const { config: approveConfig } = usePrepareContractWrite({
+    address: "0xf89c10e67b2F8bDd2a44cF4E081378e0EAA5C428",
+    abi: GogeToken1.abi,
+    functionName: "approve",
+    args:[GogeToken2.address, ethers.utils.parseUnits(balanceV1)],
+    enabled: isConnected && !ethers.utils.parseUnits(balanceV1).isZero(),
+    onSuccess(data){
+        console.log(`Prepared ${data.functionName} to approve ${balanceV1} V1 tokens for ${wallet}`)
+    },
+    onError(error) {
+        console.error('Failed to prepare approval', error);
     }
-  }, [addressWallet])
-  
-  async function migrate() {
+  })
 
-    if(web3Provider !== null && wallet.length > 0) {
-      // check balance
-      const balV1:ethers.BigNumber = await contractV1.balanceOf(wallet);
-      // check price feed given the balance
+  const { isSuccess: approveSuccess, writeAsync: approveTx } = useContractWrite({
+    ...approveConfig,
+    mode: "prepared",
+    onSuccess() {
+        console.log(`Successfully approved GogeV1 balance for ${wallet}`)
+    },
+    onError(error) {
+        console.error('Failed to perform approval', error)
+    }
+  })
 
-      // if greater than equivalent amount of $2, replace zero with price feed results
-      if(balV1.gt(0)) {
-        const signer = web3Provider.getSigner();
-        const approvalTx = await contractV1.connect(signer).approve(GogeToken2.address, balV1);
-        setStatus("Approving");
-        // wait until transaction is mined.
-        await approvalTx.wait();
+  const { config: migrateConfig } = usePrepareContractWrite({
+    address: "0x1618efC9867F3Bd7D2bf80ce5f7E6174Fd3bEf96",
+    abi: GogeToken2.abi,
+    functionName: "migrate",
+    enabled: isConnected && approveSuccess, // unless disabled, this will always run immediately!
+    onSuccess(data){
+        console.log(`Prepared ${data.functionName} to migrate tokens for ${wallet}`)
+    },
+    onError(error) {
+        console.error('Failed to prepare migration', error);
+    }
+  })
 
-        setStatus("WaitingConfirmation");
-        
-        const migrateTx = await contractV2.connect(signer).migrate();
-        // wait until transaction is mined.
+  const { isSuccess: migrateSuccess, writeAsync: migrateTx } = useContractWrite({
+    ...migrateConfig,
+    mode: "prepared",
+    onSuccess() {
+        console.log(`Successfully migrated V1 tokens to V2 for ${wallet}`)
+    },
+    onError(error) {
+        console.error('Failed to perform migration', error)
+    }
+  })
 
-        setStatus("Migrating");
-
-        await migrateTx.wait();
-
-        const balV2 = ethers.utils.formatEther(await contractV2.balanceOf(wallet));
-        setBalanceV2(balV2);
-        setMigrated(true);
-        setStatus("Migrated")
-      } else {
-        setBalanceV1("Insufficient GOGE V1 balance!");
-      }
-    }*/
-  }
+  useEffect(() => {
+    if(!isConnected) {
+        console.log('Please connect a wallet to the application!')
+        setBalanceV1("0.0")
+        setBalanceV2("0.0")
+    }
+  }, [isConnected])
 
   return (
       <>
-        {migrated ? <Confetti /> : null}
+        {migrateSuccess ? <Confetti /> : null}
 
         <nav className="bg-white font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-1 px-6 goge-navbar shadow sm:items-baseline w-full">
               <div className="mb-2 sm:mb-0">
@@ -168,10 +140,10 @@ const Migration = () => {
           <div className="flex pt-28 bg-white">
               <div className="m-auto w-1/6 xs:w-5/6 sm:w-4/6 md:w-1/6 lg:w-1/6 xl:w-1/6">
                   <div className="migrate-box">
-                      <div className="px-4 py-5 sm:p-6">
+                      <div className="m-auto px-4 py-5 sm:p-6">
                           <div className="mt-2 max-w-xl text-sm">
                             {
-                              migrated ? 
+                              migrateSuccess && ethers.utils.parseUnits(balanceV1).isZero() ? 
                                 <div>
                                   <h1 className="font-bold text-purple-700">GOGE V2 Balance:</h1>
                                   <h2 className="text-truncate text-purple-700">{balanceV2}</h2>
@@ -183,31 +155,28 @@ const Migration = () => {
                                 </div>
                             }
                           </div>
-                          {/*<div className="mt-5 flex flex-col items-center">
-                              <button
-                              className="inline-flex m-auto content-center migrate-button px-4 py-2 sm:text-sm"
-                              type="button"
-                              onClick={connected ? migrate : connect}
-                              disabled={(status == 'ConnectedNoTokens') ? true : (status == 'Approving') ? true : (status == 'WaitingConfirmation') ? true : (status == 'Migrating') ? true : (status == 'Migrated') ? true : false}
-                              >
-                                { 
-                                  (!status) ? "Connect Wallet" : 
-                                  (status == 'ConnectedNoTokens') ? "No Tokens To Migrate" : 
-                                  (status == 'ConnectedTokens') ? 'Migrate' : 
-                                  (status == 'Approving') ? 'Approving...' : 
-                                  (status == 'WaitingConfirmation') ? 'Please Approve Migrate in MetaMask' : 
-                                  (status == 'Migrating') ? 'Migrating...' : 
-                                  (status == 'Migrated') ? 'Tokens Migrated!' : 
-                                   ''}
-                              </button>
-                                </div>*/}
-                          <div className="pt-4 flex items-center">
-                            <Web3Button icon="show" label="Connect Wallet" balance="show" />
+                          <div className="pt-4 flex content-center">
+                            <button className="inline-flex m-auto migrate-button px-4 py-2 sm:text-sm"
+                                onClick={async () => {
+                                    await approveTx?.()
+                                    .catch(() => {
+                                        console.error('User rejected apporval!')
+                                    })
+                                    .then(() =>
+                                        migrateTx?.()
+                                        .catch(() => {
+                                            console.error('User rejected migration!')
+                                        })
+                                    )
+                                }}
+                            >
+                                Migrate Tokens
+                            </button>
                           </div>
                       </div>
                   </div>
                   <br></br>
-                  <div className="text-center text-sm">
+                  <div className="text-center text-sm text-purple-500">
                       Disclaimer: You must be holding more than $2 of the v1 token to migrate
                   </div>
               </div>
@@ -218,3 +187,22 @@ const Migration = () => {
 }
 
 export default Migration;
+
+/*<div className="mt-5 flex flex-col items-center">
+    <button
+    className="inline-flex m-auto content-center migrate-button px-4 py-2 sm:text-sm"
+    type="button"
+    onClick={connected ? migrate : connect}
+    disabled={(status == 'ConnectedNoTokens') ? true : (status == 'Approving') ? true : (status == 'WaitingConfirmation') ? true : (status == 'Migrating') ? true : (status == 'Migrated') ? true : false}
+    >
+    { 
+        (!status) ? "Connect Wallet" : 
+        (status == 'ConnectedNoTokens') ? "No Tokens To Migrate" : 
+        (status == 'ConnectedTokens') ? 'Migrate' : 
+        (status == 'Approving') ? 'Approving...' : 
+        (status == 'WaitingConfirmation') ? 'Please Approve Migrate in MetaMask' : 
+        (status == 'Migrating') ? 'Migrating...' : 
+        (status == 'Migrated') ? 'Tokens Migrated!' : 
+        ''}
+    </button>
+</div>*/
